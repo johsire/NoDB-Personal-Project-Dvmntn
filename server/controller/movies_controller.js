b4a7ae5a3d4c40d76ffd94f8f24f9025140b4871
Josiah Maina <josiah5115@gmail.com>
2018-07-06 19:27:17 -0600

    movie controller input

--- server/controller/movies_controller.js
+++ server/controller/movies_controller.js
@@ -2,7 +2,13 @@ let movies = [];
 let id = 0;
 
 module.exports = {
+  read: ( req, res ) => {
+    res.status(200).send( movies );
+  },
   create: ( req, res ) => {
-    
-  }
+
+},
+  update: ( req, res ) => {
+
+}
 }

0ddbb24274602d9dbdc74d8667507067e2b440b7
Josiah Maina <josiah5115@gmail.com>
2018-07-06 20:31:39 -0600

    nodemon bug half fix

--- server/controller/movies_controller.js
+++ server/controller/movies_controller.js
@@ -4,11 +4,5 @@ let id = 0;
 module.exports = {
   read: ( req, res ) => {
     res.status(200).send( movies );
-  },
-  create: ( req, res ) => {
-
-},
-  update: ( req, res ) => {
-
-}
+  }
 }
--- server/server.js
+++ server/server.js
@@ -1,25 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const mc = require( __dirname + '/controllers/movies_controller');
+const mc = require('./controller/movies_controller.js'); 
 
 
 const app = express();
 
 // app.use() --> middleware that runs for Every request
 app.use(bodyParser.json());
-app.use( express.static( __dirname + '/../public/build' ) );
 
 // ======>>ENDPOINTS======>>>
 // app.get('/api/user, (req, res) => {})
 const baseURL = "/api/movies";
-app.post( baseURL, mc.create );
+//app.post( baseURL, mc.create );
 app.get( baseURL, mc.read );
-app.put( `${baseURL}/:id`, mc.update );
-app.delete( `${baseURL}/:id`, mc.delete );
+// app.put( `${baseURL}/:id`, mc.update );
+// app.delete( `${baseURL}/:id`, mc.delete );
 
 
-let { SERVER_PORT } = process.env;
+const PORT = 5353;
 app.listen(PORT, () => {
   console.log('Listening to port' + PORT);
 });  

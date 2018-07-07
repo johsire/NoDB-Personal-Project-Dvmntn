42410ec880e78926deed80fe2f3677646eb0c88f
Josiah Maina <josiah5115@gmail.com>
2018-07-06 18:54:37 -0600

    more server input

--- server/server.js
+++ server/server.js
@@ -8,17 +8,18 @@ const app = express();
 
 // app.use() --> middleware that runs for Every request
 app.use(bodyParser.json());
-
 app.use( express.static( __dirname + '/../public/build' ) );
+
+// ======>>ENDPOINTS======>>>
+// app.get('/api/user, (req, res) => {})
+const baseURL = "/api/movies";
+app.post( baseURL, mc.create );
+app.get( baseURL, mc.read );
+app.put( `${baseURL}/:id`, mc.update );
+app.delete( `${baseURL}/:id`, mc.delete );
 
 
 let { SERVER_PORT } = process.env;
 app.listen(PORT, () => {
   console.log('Listening to port' + PORT);
-});    
-
-// ENDPOINTS=====>>
-
-// app.get('/api/user, (req, res) => {})
-
-// ENDPOINTS=====>>
+});  
